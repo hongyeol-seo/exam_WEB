6e3eb362f273435f0c5c14845376b9b9ae5cfe8e
hong <napolehong92@naver.com>
2023-01-10 16:18:54 +0900

    23.01.10_html

--- t1.py
+++ t1.py
@@ -1,6 +1,12 @@
+# import pandas as pd
+# # import lxml
+
+# df = pd.read_html('https://ko.wikipedia.org/wiki/%EC%9D%B8%EA%B5%AC%EC%88%9C_%EB%82%98%EB%9D%BC_%EB%AA%A9%EB%A1%9D', header = 0)[0] 
+
+# print(df.head())
+
 import pandas as pd
-# import lxml
+dict_data = {'a': 1, 'b': 2, 'c': 3}
+sr = pd.Series(dict_data)
 
-df = pd.read_html('https://ko.wikipedia.org/wiki/%EC%9D%B8%EA%B5%AC%EC%88%9C_%EB%82%98%EB%9D%BC_%EB%AA%A9%EB%A1%9D', header = 0)[0] 
-
-print(df.head())
+print(sr)
